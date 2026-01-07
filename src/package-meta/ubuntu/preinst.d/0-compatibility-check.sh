15a2fe70b3ef1d99059974f28092ab044e7d1842
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-01-07 15:56:10 +0900

    Create 0-compatibility-check.sh

---
+++ src/package-meta/ubuntu/preinst.d/0-compatibility-check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if current system is installed in btrfs filesystem
+# If not, it is incompatible
+if [ "$(findmnt -n -o FSTYPE /)" != "btrfs" ]; then
+    echo "Error: The root filesystem is not BTRFS. This system is incompatible."
+    exit 1
+fi
+
+# Check if /boot is not mounted as separate partition
+# If so, then it is incompatible (Meaning: /boot MUST be a separate partition)
+if ! mountpoint -q /boot; then
+    echo "Error: /boot is not mounted as a separate partition. This system is incompatible."
+    exit 1
+fi
+
+echo "System compatibility check passed."
+exit 0
