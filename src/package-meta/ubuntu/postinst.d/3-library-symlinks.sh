4cb45a87e84fd30c4a96a3b5ceabcb30a1f4d0eb
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-01-11 02:31:25 +0900

    Create 3-library-symlinks.sh

---
+++ src/package-meta/ubuntu/postinst.d/3-library-symlinks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Symbolic link all .py files from /opt/aqua/sys/lib/python/ to /usr/lib/python3/dist-packages/aqua/
+SOURCE_DIR="/opt/aqua/sys/lib/python/"
+TARGET_DIR="/usr/lib/python3/dist-packages/"
+
+mkdir -p "$TARGET_DIR"
+find "$SOURCE_DIR" -name "*.py" | while read -r file; do
+    relative_path="${file#$SOURCE_DIR}"
+    target_path="$TARGET_DIR$relative_path"
+    target_dir="$(dirname "$target_path")"
+    mkdir -p "$target_dir"
+    ln -sf "$file" "$target_path"
+done
+echo "Symbolic links for .py files created from $SOURCE_DIR to $TARGET_DIR"
+
+unset SOURCE_DIR
+unset TARGET_DIR
+unset file
+unset relative_path
+unset target_path
+unset target_dir
+
