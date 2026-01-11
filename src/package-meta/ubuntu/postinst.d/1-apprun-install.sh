d9d4d5d41805df735506c8749343556b8f532e4c
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-01-11 15:57:56 +0900

    Typo fix

--- src/package-meta/ubuntu/postinst.d/1-apprun-install.sh
+++ src/package-meta/ubuntu/postinst.d/1-apprun-install.sh
@@ -9,4 +9,4 @@ ln -s /usr/local/sbin/apprun-prepare.sh /usr/local/bin/apprun-prepare
 ln -s /usr/local/sbin/dictionary.py /usr/local/bin/dictionary
 
 /usr/local/sbin/apprun-prepare.sh "/opt/aqua/services/me.hysong.aqua.services.apprundropin.apprun"
-/opt/aqya/sys/sbin/services.sh enable me.hysong.aqua.services.apprundropin
+/opt/aqua/sys/sbin/services.sh enable me.hysong.aqua.services.apprundropin
