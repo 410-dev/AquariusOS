9d5fbf7b3d6c50480c58f673e77445a614ad85dc
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-01-14 15:51:38 +0900

    Update 3-library-symlinks.sh

--- src/package-meta/ubuntu/postinst.d/3-library-symlinks.sh
+++ src/package-meta/ubuntu/postinst.d/3-library-symlinks.sh
@@ -5,12 +5,13 @@ SOURCE_DIR="/opt/aqua/sys/lib/python/"
 TARGET_DIR="/usr/lib/python3/dist-packages/"
 
 mkdir -p "$TARGET_DIR"
-find "$SOURCE_DIR" -name "*.py" | while read -r file; do
+find "$SOURCE_DIR" -name *.py | while read -r file; do
     relative_path="${file#$SOURCE_DIR}"
     target_path="$TARGET_DIR$relative_path"
     target_dir="$(dirname "$target_path")"
     mkdir -p "$target_dir"
     ln -sf "$file" "$target_path"
+    echo "Created symlink: $target_path -> $file"
 done
 echo "Symbolic links for .py files created from $SOURCE_DIR to $TARGET_DIR"
 
