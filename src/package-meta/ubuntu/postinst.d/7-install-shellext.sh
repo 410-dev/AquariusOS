37e1520ff6d3a0741f232a6191c6e2b2bce9bbdc
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-01-14 09:48:46 +0900

    Update 7-install-shellext.sh

--- src/package-meta/ubuntu/postinst.d/7-install-shellext.sh
+++ src/package-meta/ubuntu/postinst.d/7-install-shellext.sh
@@ -2,7 +2,7 @@
 
 if ! is_marked "AquariusOSSetupDone.ShellExtensions.var" ; then
     # Install system-wide hook for bash and POSIX shells
-    echo "source /etc/profile.d/aquariusos.sh" >> /etc/bash.bashrc
+    echo "source /etc/profile.d/aqua.sh" >> /etc/bash.bashrc
     mark "AquariusOSSetupDone.ShellExtensions.var"
 else
     echo "Marked as setup done. Skipping..."
