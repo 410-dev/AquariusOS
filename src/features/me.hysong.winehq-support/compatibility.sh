22f88fb46957118aa7f0fefcd4929f4ad2dd211b
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-01-15 10:13:45 +0900

    Removed me.hysong.winehq-support

--- src/features/me.hysong.winehq-support/compatibility.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if current machine is exclusively x64 (amd64 only)
-if lscpu | grep -q "Architecture: *x86_64" && ! lscpu | grep -q "Architecture: *i[3-6]86"; then
-    exit 0
-else
-    echo "Error: This feature is only compatible with x64 (amd64) architecture."
-    exit 1
-fi
--- src/features/me.hysong.winehq-support/enable.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -pm755 /etc/apt/keyrings
-dpkg --add-architecture i386
-wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
-wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
-apt install winehq-stable winehq-stable-i386 wine-stable-amd64 wine-stable-i386 -y
