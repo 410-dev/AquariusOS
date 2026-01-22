20ec632689e8d44d1ece853f172eda97a78114ff
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-01-22 12:25:01 +0900

    InstallmentScript privilege update on enable

--- src/features/me.hysong.SnapshotSupport/enable.sh
+++ src/features/me.hysong.SnapshotSupport/enable.sh
@@ -9,6 +9,11 @@ apt install overlayroot -y
 # Add btrfs module to grub
 grub-install --modules=btrfs
 
+chown root "$1/stages/stage1.sh"
+chown root "$1/stages/stage2.sh"
+chmod 755 "$1/stages/stage1.sh"
+chmod 755 "$1/stages/stage2.sh"
+
 /opt/aqua/sys/sbin/preboot.sh SetNextInstallmentScript /opt/aqua/features/me.hysong.SnapshotSupport/stages/stage1.sh
 
 echo "Feature enablement will be applied on next reboot. Several reboots may be required to complete the process."
--- src/features/snapshot/enable.sh
+++ src/features/snapshot/enable.sh
@@ -13,6 +13,15 @@ grub-install --modules=btrfs
 cp -r "$1/patch"/* /
 ln -sf /opt/aqua/sys/lib/python/libsnapshot.py /usr/lib/python3/dist-packages/libsnapshot.py
 
+# Set permission of stages
+# file is owned by root and not writable by group/others
+chown root "$1/stages/stage1.sh"
+chown root "$1/stages/stage2.sh"
+chown root "$1/stages/stage3.sh"
+chmod 755 "$1/stages/stage1.sh"
+chmod 755 "$1/stages/stage2.sh"
+chmod 755 "$1/stages/stage3.sh"
+
 /opt/aqua/sys/sbin/preboot.sh SetNextInstallmentScript "$1/stages/stage1.sh"
 
 echo "Feature enablement will be applied on next reboot. Several reboots may be required to complete the process."
