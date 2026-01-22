57530d9cc44076ddd40d75851e532da41c938e26
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-01-22 12:23:10 +0900

    Enforce preboot v4 for security

--- src/package-meta/ubuntu/postinst.d/11-preboot-enablement.sh
+++ src/package-meta/ubuntu/postinst.d/11-preboot-enablement.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# By default, enable preboot version 3 and Bgrt
-/opt/aqua/sys/sbin/preboot.sh EnablePreboot 3
+# By default, enable preboot version 4 and Bgrt
+/opt/aqua/sys/sbin/preboot.sh EnablePreboot 4
 /opt/aqua/sys/sbin/preboot.sh EnableBgrt
