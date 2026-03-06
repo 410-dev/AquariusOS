982b05e398088f01f0272f1c90f15a9007379f85
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-03-07 06:51:36 +0900

    Submodule updates

--- libraries/extension/java/libcodablejdbc/lib/libcodablejson/src/main/java/me/hysong/libcodablejson/JsonCodableException.java
+++
@@ -1,7 +0,0 @@
-package me.hysong.libcodablejson;
-
-public class JsonCodableException extends RuntimeException {
-    public JsonCodableException(String message) {
-        super(message);
-    }
-}
