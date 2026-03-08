c5c96bf40ca94f9c02e919ada4399a26c5e291df
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-03-08 18:21:34 +0900

    Service naming changes

--- package-meta/ubuntu/postinst.d/10-linux-shell-compliance-enablement.sh
+++ package-meta/ubuntu/postinst.d/10-linux-shell-compliance-enablement.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 {{SYS_CMDS}}/reg.sh root install {{RESOURCES}}/nixshcompliance/registry-template.regtree
-#/usr/local/sbin/apprun-prepare.sh {{OPT_SERVICES}}/me.hysong.services.nixshcompliance.apprun # This is enabled by 5-prepare-bundles.sh
-{{SYS_CMDS}}/services.sh enable me.hysong.services.nixshcompliance
+#/usr/local/sbin/apprun-prepare.sh {{OPT_SERVICES}}/nixshcompliance.apprun # This is enabled by 5-prepare-bundles.sh
+{{SYS_CMDS}}/services.sh enable nixshcompliance
