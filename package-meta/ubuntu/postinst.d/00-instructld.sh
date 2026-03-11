2c43a37a9136b491092a123bde4d5280dce25417
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-03-11 17:36:30 +0900

    Emulate X11 display variable to :0

--- package-meta/ubuntu/postinst.d/00-instructld.sh
+++ package-meta/ubuntu/postinst.d/00-instructld.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 source {{SYS_SHELLLIBS}}/guarding.shx
+
+
+# Emulate $DISPLAY for postinst script - as DBUS fails without it, and we need it to show the notification.
+if [[ -z "$DISPLAY" ]]; then
+    export DISPLAY=:0
+fi
