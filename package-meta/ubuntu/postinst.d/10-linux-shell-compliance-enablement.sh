dbf11847d54e70650a27f9dd1136093d1e249b33
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-03-11 17:36:55 +0900

    nixshcompliance separate enable is unnecessary now

--- package-meta/ubuntu/postinst.d/10-linux-shell-compliance-enablement.sh
+++ package-meta/ubuntu/postinst.d/10-linux-shell-compliance-enablement.sh
@@ -2,4 +2,4 @@
 
 {{SYS_CMDS}}/reg.sh root install {{RESOURCES}}/nixshcompliance/registry-template.regtree
 #/usr/local/sbin/apprun-prepare.sh {{OPT_SERVICES}}/nixshcompliance.apprun # This is enabled by 5-prepare-bundles.sh
-{{SYS_CMDS}}/services.sh enable nixshcompliance
+#{{SYS_CMDS}}/services.sh enable nixshcompliance
