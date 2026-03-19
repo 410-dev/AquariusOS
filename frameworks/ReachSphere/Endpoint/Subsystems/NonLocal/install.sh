b255dbb15821a037272ad4d80d8b61189e348d16
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-03-19 15:44:19 +0900

    Create nonlocal/install.sh

---
+++ frameworks/ReachSphere/Endpoint/Subsystems/NonLocal/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if user is root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+# Assuming ndauthhook.py is in /usr/local/bin
+ndauthhook="{{SYS_FRAMEWORKS}}/ReachSphereEndpoint/Subsystems/NonLocal/pam.py"
+
+chmod +x "$ndauthhook"
+chown root:root "$ndauthhook"
+
+# Prepend the hook to /etc/pam.d/gdm-password
+pam_file="/etc/pam.d/gdm-password"
+hook_line="auth required pam_exec.so expose_authtok quiet ${ndauthhook}"
+
+
+if ! grep -Fxq "$hook_line" "$pam_file"; then
+    echo "Adding authentication hook to $pam_file"
+    sed -i "1i $hook_line" "$pam_file"
+else
+    echo "Authentication hook already present in $pam_file"
+fi
+
